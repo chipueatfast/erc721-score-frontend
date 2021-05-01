afb35cb418c926c62082a2feb00c6c518d6b5c7a
phat <phat.pham@nfq.asia>
2021-05-01 17:04:54 +0700

    feat: can mint token and listen with once

--- src/services/mintAToken.ts
+++ src/services/mintAToken.ts
@@ -1,13 +1,30 @@
 import { getContract } from 'smart-contract/erc721-score';
 
-export function mintAToken({
+export async function mintAToken({
     toAddress,
     scoreHash,
 }: {
     toAddress: string,
     scoreHash: string,
-}) {
-    getContract().methods.mint(toAddress, scoreHash).send({
-        from: '0x8a4e1258e898e526046EB8CebAAE23B655B9783b',
-    });
+}): Promise<{
+    reponse?: any;
+    errorMessage?: string;
+}> {
+    return new Promise((resolve) => {
+        try {
+            getContract().methods.mint(toAddress, scoreHash).send({
+                from: '0x8a4e1258e898e526046EB8CebAAE23B655B9783b',
+            });
+            getContract().once('Transfer', (err, data) => {
+                resolve({
+                    reponse: data.returnValues,
+                })
+            })
+
+        } catch (error) {
+            return resolve({
+                errorMessage: error.message,
+            });
+        }
+    })
 }
--- src/smart-contract/erc721-score/index.ts
+++ src/smart-contract/erc721-score/index.ts
@@ -1,13 +1,18 @@
-import Web3EthContract, { Contract } from 'web3-eth-contract';
+import { getWeb3 } from 'GlobalContext';
+import * as Web3EthContract from 'web3-eth-contract';
+
+const ERC721_CONTRACT_ADDRESS = '0xa34E6C6354771D74dCaACc077eC1C9e2Dd2C41De';
 
 const erc721ScoreJson = require('./ERC721PresetMinterPauserAutoId.json');
 
-let contract: Contract;
+let contract: Web3EthContract.Contract;
 
-export function getContract(): Contract {
-    if (!contract) {
+export function getContract(): Web3EthContract.Contract {
+    if (!contract && !!getWeb3()) {
         console.log(Web3EthContract);
-        contract = new (Web3EthContract as any)(erc721ScoreJson.abi);
+        (Web3EthContract as any).setProvider(getWeb3().eth.currentProvider);
+
+        contract = new (Web3EthContract as any)(erc721ScoreJson.abi, ERC721_CONTRACT_ADDRESS);
     }
     return contract;
 }
