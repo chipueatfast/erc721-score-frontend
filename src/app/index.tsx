7e33ab72f058bc6a9adb8e14b88650a8f031981f
phat <phat.pham@nfq.asia>
2021-05-09 12:39:42 +0700

    feat: set up firebase provider

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import { FirebaseDatabaseProvider } from "@react-firebase/database";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
-
+import firebase from 'firebase/app';
+import 'firebase/database';
+import { config } from 'firebase-service/config';
 import { JudgePage } from 'pages/JudgePage';
 import { askForConnect } from 'services/askForConnect';
 import { getWeb3 } from 'GlobalContext';
 import './index.d';
 import { CandidatePage } from 'pages/CandidatePage';
-
 
 function App() {
   const [userAddress, setUserAddress] = React.useState<string>('');
@@ -24,35 +26,39 @@ function App() {
       });
     });
   }, []);
+  console.log(FirebaseDatabaseProvider);
 
   return (
-    <Router>
-      <div className="App">
-        <nav>
-          <ul>
-              <li>
-                <Link to="/judge">Judge</Link>
-              </li>
-              <li>
-                <Link to="/candidate">Candidate</Link>
-              </li>
-          </ul>
-        </nav>
-        <span>
-          {userAddress}
-        </span>
-        <Switch>
-          <Route path='/judge'>
-            <JudgePage
-              userAddress={userAddress}
-            />
-          </Route>
-          <Route path='/candidate'>
-            <CandidatePage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <FirebaseDatabaseProvider firebase={firebase} {...config}>
+      <Router>
+        <div className="App">     
+              <nav>
+                <ul>
+                    <li>
+                      <Link to="/judge">Judge</Link>
+                    </li>
+                    <li>
+                      <Link to="/candidate">Candidate</Link>
+                    </li>
+                </ul>
+              </nav>
+              <span>
+                {userAddress}
+              </span>
+              <Switch>
+                <Route path='/judge'>
+                  <JudgePage
+                    userAddress={userAddress}
+                  />
+                </Route>
+                <Route path='/candidate'>
+                  <CandidatePage />
+                </Route>
+              </Switch>      
+          </div>
+        </Router>
+      </FirebaseDatabaseProvider>
+  
   );
 }
 
